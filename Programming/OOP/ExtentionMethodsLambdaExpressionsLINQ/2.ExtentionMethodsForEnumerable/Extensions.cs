647fefddf56fad07e6ec06579d79c9f7077d9122
Petar Yankov <peshopit@yahoo.com>
2013-03-02 21:47:53 +0200

    Homework

---
+++ Programming/OOP/ExtentionMethodsLambdaExpressionsLINQ/2.ExtentionMethodsForEnumerable/Extensions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2.ExtentionMethodsForEnumerable
+{
+    static class Extensions
+    {
+        // Sum method
+        public static decimal Sum<T>(this IEnumerable<T> enumeration)
+            where T: struct, IComparable<T>
+        {
+            if (enumeration.ToList().Count == 0)
+            {
+                throw new ArgumentException("Cannot find the sum of an empty enumeration!");
+            }
+            decimal sum = 0;
+            try
+            {
+                foreach (var element in enumeration)
+                {
+                    sum = sum + Convert.ToDecimal(element);
+                }
+            }
+            catch (OverflowException)
+            {
+                throw new OverflowException("The sum is bigger than Maximal Decimal number!");
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("The element cannot be converted to Decimal number!");
+            }
+            return sum;
+        }
+
+        //Product method
+        public static decimal Product<T>(this IEnumerable<T> enumeration)
+            where T : struct, IComparable<T>
+        {
+            if (enumeration.ToList().Count == 0)
+            {
+                throw new ArgumentException("Cannot find the product of an empty enumeration!");
+            }
+            decimal product = 1;
+            try
+            {
+                foreach (var element in enumeration)
+                {
+                    product = product * Convert.ToDecimal(element);
+                }
+            }
+            catch (OverflowException)
+            {
+                throw new OverflowException("The product is bigger than Maximal Decimal number!");
+            }
+            return product;
+        }
+
+        // Min method
+        public static decimal Min<T>(this IEnumerable<T> enumeration)
+            where T : struct, IComparable<T>
+        {
+            if (enumeration.ToList().Count == 0)
+            {
+                throw new ArgumentException("Cannot find the Minimal element of an empty enumeration!");
+            }
+            decimal minimal = Decimal.MaxValue;
+            try
+            {
+                foreach (var element in enumeration)
+                {
+                    if (minimal > Convert.ToDecimal(element))
+                    {
+                        minimal = Convert.ToDecimal(element);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("The element cannot be converted to Decimal number!");
+            }
+            return minimal;
+        }
+
+        // Max method
+        public static decimal Max<T>(this IEnumerable<T> enumeration)
+            where T : struct, IComparable<T>
+        {
+            if (enumeration.ToList().Count == 0)
+            {
+                throw new ArgumentException("Cannot find the Minimal element of an empty enumeration!");
+            }
+            decimal maximal = Decimal.MinValue;
+            try
+            {
+                foreach (var element in enumeration)
+                {
+                    if (maximal < Convert.ToDecimal(element))
+                    {
+                        maximal = Convert.ToDecimal(element);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("The element cannot be converted to Decimal number!");
+            }
+            return maximal;
+        }
+
+
+        // Average method
+        public static decimal Average<T>(this IEnumerable<T> enumeration)
+            where T : struct, IComparable<T>
+        {
+            if (enumeration.ToList().Count == 0)
+            {
+                throw new ArgumentException("Cannot find the Average of an empty enumeration!");
+            }
+            decimal average = enumeration.Sum()/enumeration.ToList().Count;
+            return average;
+        }
+    }
+}
---
+++ Programming/OOP/ExtentionMethodsLambdaExpressionsLINQ/2.ExtentionMethodsForEnumerable/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2.ExtentionMethodsForEnumerable
+{
+    class Tests
+    {
+        static void Main()
+        {
+            List<double> newList = new List<double>();
+            newList.Add(1);
+            newList.Add(2);
+            newList.Add(3);
+            newList.Add(4);
+            newList.Add(5);
+            Console.WriteLine("Elements:");
+            for (int i = 0; i < newList.Count; i++)
+            {
+                Console.WriteLine("newList[{0}] = {1}",i,newList[i]);
+            }
+
+            //Try the Sum method
+            Console.WriteLine("\nThe sum of all the elements is = {0}",newList.Sum()); //15
+
+            //Try the Product method
+            Console.WriteLine("\nThe product of all the elements is = {0}", newList.Product()); //120
+
+            //Try the Max method
+            Console.WriteLine("\nThe maximal of all the elements is = {0}", newList.Max()); //5
+
+            //Try the Min method
+            Console.WriteLine("\nThe minimal of all the elements is = {0}", newList.Min()); //1
+
+            //Try the Average method
+            Console.WriteLine("\nThe average of all the elements is = {0}", newList.Average()); //3
+        }
+    }
+}
