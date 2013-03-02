5b2c0987cb491ce0dba244f1374fc9a62b7ce99f
Petar Yankov <peshopit@yahoo.com>
2013-03-02 20:07:44 +0200

    Homework

---
+++ Programming/OOP/ExtentionMethodsLambdaExpressionsLINQ/1.StringBuilderSubstring/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.StringBuilderSubstring
+{
+    public static class Extensions
+    {
+        public static string Substring(this StringBuilder str, int startIndex, int length)
+        {
+            if (startIndex < 0 || startIndex > str.Length-1)
+            {
+                throw new ArgumentException("The substring start index is out of range of the string!");
+            }
+            if (length < 0 || length > str.Length - startIndex)
+            {
+                throw new ArgumentException("The specified substring length is invalid!");
+            }
+            char[] newCharArray = new char[length];
+            str.CopyTo(startIndex,newCharArray, 0, length);
+            return new string (newCharArray);
+        }
+
+        public static string Substring(this StringBuilder str, int startIndex)
+        {
+            if (startIndex < 0 || startIndex > str.Length - 1)
+            {
+                throw new ArgumentException("The substring start index is out of range of the string!");
+            }
+            char[] newCharArray = new char[str.Length - startIndex];
+            str.CopyTo(startIndex, newCharArray, 0, str.Length - startIndex);
+            return new string(newCharArray);
+        }
+    }
+}
---
+++ Programming/OOP/ExtentionMethodsLambdaExpressionsLINQ/1.StringBuilderSubstring/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.StringBuilderSubstring
+{
+    class Tests
+    {
+        static void Main()
+        {
+            //Try the Substring method on class String
+            string newString = "Pesho Plet Plete";
+            Console.WriteLine("Try the Substring method on class String:");
+            Console.WriteLine(newString.Substring(2));
+            Console.WriteLine(newString.Substring(2, 2));
+
+            //Try the Substring method on class StringBuilder
+            Console.WriteLine("\nTry the Substring method on class StringBuilder:");
+            StringBuilder newStringBuilder = new StringBuilder("Pesho Plet Plete");
+            Console.WriteLine(newStringBuilder.Substring(2));
+            Console.WriteLine(newStringBuilder.Substring(2, 2));
+        }
+    }
+}
